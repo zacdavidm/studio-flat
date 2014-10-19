d217db162cccf70dba410cc6fa517ef9893d091f
zacdavidm <zacdavidm@gmail.com>
2014-10-19 17:10:39 -0400

    add support for linked logo in topbar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -12,7 +12,12 @@
       <?php endif; ?>
       <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
         <ul class="title-area">
-          <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
+          <?php if ($linked_logo): ?>
+            <li class="name"><?php print $linked_logo;?></li>
+            <h1 class="element-invisible"><?php print $linked_site_name; ?></h1>
+          <?php else: ?>
+            <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
+          <?php endif; ?>
           <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
         </ul>
         <section class="top-bar-section">
